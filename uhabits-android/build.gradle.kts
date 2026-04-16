935bfae7df37cf173cf9e95a742f1846ce073836
kayyum-5b803176 <kayyum954f@gmail.com>
2026-04-16 18:51:21 +0530

    update version

--- uhabits-android/build.gradle.kts
+++ uhabits-android/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 
     defaultConfig {
         versionCode = 20202
-        versionName = "2.2.2"
+        versionName = "2.3.0"
         minSdk = 28
         targetSdk = 34
         // targetSdkPreview = "VanillaIceCream"
